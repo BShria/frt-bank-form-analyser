303e0073a8e14a1de6aa1507f8e73859cbe11bc5
Shria Banerjee <shriabanerjee@gmail.com>
2022-06-14 16:30:54 +0530

    Final commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const fs = require("fs");
 const multer = require('multer');
 const { AzureKeyCredential, DocumentAnalysisClient } = require("@azure/ai-form-recognizer");
@@ -8,6 +9,7 @@ const { AzureKeyCredential, DocumentAnalysisClient } = require("@azure/ai-form-r
 const app = express();
 
 app.use(express.json());
+app.use(express.static("./client/build"));
 app.use(cors());
 
 // for parsing multipart/form-data
@@ -49,10 +51,14 @@ app.post("/upload", (req, res) => {
     }
     );
   }
-})
+});
 
-let port = process.env.port | 3001;
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+let port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log("Server is running in port 3001");
+  console.log("Server is running in port " + port);
 });
